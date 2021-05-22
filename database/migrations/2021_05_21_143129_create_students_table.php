1983878ffbaebd67607df219c63296eaa16a859c
uzain <uzainali01@gmail.com>
2021-05-22 18:19:05 +0500

    other three folders

---
+++ database/migrations/2021_05_21_143129_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('course');
+            $table->string('mobile_no');
+            $table->string('father_name');
+            $table->string('father_contact');
+            $table->string('cnic_no');
+            $table->integer('age');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
